58b053189f6277dc41ed42cd91bfcfe489c39d08
migromarj <miguelromeroarjona@gmail.com>
2023-02-18 21:17:22 +0100

    feat(#003): Configure ESLint and add Prettier to the project
    The warnings that these tools alerted after their configuration have been fixed.

--- pages/tests/index.js
+++ pages/tests/index.js
@@ -1,33 +1,38 @@
 export async function getServerSideProps() {
 	try {
-		const res = await fetch("http://localhost:8000/api/tests");
-		const tests = await res.json();
+		const res = await fetch('http://localhost:8000/api/tests')
+		const tests = await res.json()
 
 		return {
 			props: {
 				tests,
 			},
-		};
-	}catch (err) {
+		}
+	} catch (err) {
 		const tests = []
 		return {
 			props: {
 				tests,
 			},
-		};
+		}
 	}
 }
 
 export default function Test({ tests }) {
 	return (
-		<div className="m-4">
-			<h1 className="text-3xl font-bold underline">Test</h1>
-			{tests.length === 0 && <h2 className="text-2xl font-bold">No se encontraron tests</h2>}
-			{tests.length > 0 && tests.map((test) => (
-				<div key={test.pk}>
-					<h2 className="text-2xl font-bold">{test.pk} - {test.name}</h2>
-				</div>
-			))}
+		<div className='m-4'>
+			<h1 className='text-3xl font-bold underline'>Test</h1>
+			{tests.length === 0 && (
+				<h2 className='text-2xl font-bold'>No se encontraron tests</h2>
+			)}
+			{tests.length > 0 &&
+				tests.map(test => (
+					<div key={test.pk}>
+						<h2 className='text-2xl font-bold'>
+							{test.pk} - {test.name}
+						</h2>
+					</div>
+				))}
 		</div>
-	);
+	)
 }
